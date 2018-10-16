9f3409ff95caeebf1b01f33ee971215f69748022
Benjamin Leov <benleov@gmail.com>
2018-10-16 13:53:10 +1100

    init

---
+++ README.md
@@ -0,0 +1,96 @@
+# Alexa - Pick a path adventure
+
+# Introduction
+
+This project allows you to create your own voice based pick a path adventure which is described as JSON in Dynamo DB.
+
+
+### Pre-requisites
+
+* Node.js (> v4.3)
+* The [ask CLI](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html)
+* Register for an [AWS Account](https://aws.amazon.com/)
+* Register for an [Amazon Developer Account](https://developer.amazon.com/)
+
+# General setup
+
+1. Install ask cli & run ``` ask init ``` to link with your account
+
+1. Clone the repository.
+
+	```bash
+	$ git clone https://github.com/alexa-pap/
+	```
+
+1. Navigating into the repository's root folder.
+
+	```bash
+	$ cd alexa-pap
+	```
+
+1. Install npm dependencies by navigating into the `lambda/` directory and running the npm command: `npm install`
+
+	```bash
+	$ cd lambda/
+	$ npm install
+	```
+	
+### Repository Contents
+
+* `/lambda/` - The lambda function that processes requests from alexa
+* `/examples/sample-story.json`	- An example story stored in DynamoDB
+
+### Deployment
+
+ASK CLI will create the skill and the lambda function for you. The Lambda function will be created in ```us-east-1 (Northern Virginia)``` by default
+
+1. Deploy the skill and the lambda function in one step by running the following command:
+
+	```bash
+	$ ask deploy
+	```
+
+2. Once deployed, additional permissions need to be added to the AWS IAM role being used by the skill since it is persisting data in Amazon DynamoDB.  Navigate to the [AWS IAM console](https://console.aws.amazon.com/iam/home#/roles).
+
+	> _Note: We are adding the full access policy here for convenience.  For a production skill, you should use a more targeted policy restricting access to just the required resources.  Refer to the [DynamoDB documentation](https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/access-control-overview.html) for more details._
+
+	1. Locate the role for your skill (by default, it is named ```ask-lambda-<your skill name>```).
+	1. Click on the role, then click **Attach Policy**.
+	1. Search for **AmazonDynamoDBFullAccess** and click the check box next to it.
+	1. Click **Attach Policy**.
+
+#### If you deploy this manually (not using the ask cli), please note that:
+
+- You need to make sure that your Alexa is in the same region and using locale as what you have deployed.
+- You need to deploy the lambda that supports Alexa, such as Ireland (eu-west-1)
+
+### Creating your story
+
+- Create row in the Pick-A-Path table within dynamo db with key "story"
+- Using an editor like https://jsoneditoronline.org/ is easier than using DynamoDB to edit the story as the latter will 
+rearrange the children elements to be at the top making it difficult to work with.
+- Modify speech with https://developer.amazon.com/docs/custom-skills/speech-synthesis-markup-language-ssml-reference.html
+- An example is in ``` /examples/example-story.json ```
+
+### Testing
+
+1. To test, you need to login to Alexa Developer Console, and enable the "Test" switch on your skill from the "Test" Tab.
+
+2. Simulate verbal interaction with your skill through the command line using the following example:
+
+	```bash
+	 $ ask simulate -l en-US -t "start pick a path game"
+
+	 ✓ Simulation created for simulation id: 4a7a9ed8-94b2-40c0-b3bd-fb63d9887fa7
+	  Waiting for simulation response{
+	  "status": "SUCCESSFUL",
+	  ...
+	 ```
+
+> _Note: if you did not add the DynamoDB permission as described in the previous step, the skill will return an error and you will see an error in your CloudWatch Logs reporting:  ...```Coud not read item```...```is not authorized to perform: dynamodb:GetItem```..._
+
+3. Once the "Test" switch is enabled, your skill can be tested on devices associated with the developer account as well. Speak to Alexa from any enabled device, from your browser at [echosim.io](https://echosim.io/welcome), or through your Amazon Mobile App and say :
+
+	```text
+	Alexa, start pick a path game
+	```
---
+++ lambda/index.js
@@ -0,0 +1,369 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+/* eslint-disable  no-restricted-syntax */
+
+const Alexa = require('ask-sdk');
+const TreeModel = require('tree-model');
+const AWS = require('aws-sdk');
+const {promisify} = require('util');
+
+const FALLBACK_MESSAGE_DURING_GAME = `This skill can't help you with that.`;
+const FALLBACK_REPROMPT_DURING_GAME = `This skill can't help you with that.`;
+const FALLBACK_MESSAGE_OUTSIDE_GAME = `The skill can't help you with that.`;
+const FALLBACK_REPROMPT_OUTSIDE_GAME = 'Say yes to start the game or no to quit.';
+
+const dynamoDB = new AWS.DynamoDB();
+const dbGet = promisify(dynamoDB.getItem).bind(dynamoDB);
+
+/**
+ * Dynamo query to get the story row.
+ */
+const params = {
+    TableName: 'Pick-A-Path',
+    Key: {
+        'id' : {S: 'story'},
+    }
+};
+
+const LaunchRequest = {
+    canHandle(handlerInput) {
+        // launch requests as well as any new session, as games are not saved in progress, which makes
+        // no one shots a reasonable idea except for help, and the welcome message provides some help.
+        return handlerInput.requestEnvelope.session.new || handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    async handle(handlerInput) {
+
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const attributes = await attributesManager.getPersistentAttributes() || {};
+        if (Object.keys(attributes).length === 0) {
+            attributes.endedSessionCount = 0;
+            attributes.gameState = 'ENDED';
+        }
+
+        attributesManager.setSessionAttributes(attributes);
+
+        const reprompt = 'Say yes to start the game or no to quit.';
+
+        let data = await dbGet(params);
+
+        if(data && data.Item) {
+
+            const converted = AWS.DynamoDB.Converter.unmarshall(data.Item);
+            const tree = new TreeModel();
+            let root = tree.parse(converted);
+
+            return responseBuilder
+                .speak(root.model.initSpeak)
+                .reprompt(reprompt)
+                .getResponse();
+        } else {
+
+            // cannot find story row
+
+            // const {requestEnvelope, attributesManager, responseBuilder} = handlerInput;
+            // const sessionAttributes = attributesManager.getSessionAttributes();
+            // sessionAttributes.gameState = 'ENDED';
+            // sessionAttributes.currentNodeId = 'story';
+            // attributesManager.setPersistentAttributes(sessionAttributes);
+            // await attributesManager.savePersistentAttributes();
+
+            return responseBuilder
+                .speak("The story cannot be found. Please read the Readme file for more information.")
+                .reprompt("Say no to exit.")
+                .getResponse();
+
+        }
+    },
+};
+
+const ExitHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.CancelIntent'
+                || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak('Thanks for playing.')
+            .getResponse();
+    },
+};
+
+const SessionEndedRequest = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+const HelpIntent = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechOutput = 'You ask for help, but there is none.';
+        const reprompt = 'If you were listening you would know what to do.';
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .reprompt(reprompt)
+            .getResponse();
+    },
+};
+
+const YesIntent = {
+    canHandle(handlerInput) {
+        // only start a new game if yes is said when not playing a game.
+        let isCurrentlyPlaying = false;
+        const request = handlerInput.requestEnvelope.request;
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        if (sessionAttributes.gameState &&
+            sessionAttributes.gameState === 'STARTED') {
+            isCurrentlyPlaying = true;
+        }
+
+        return !isCurrentlyPlaying && request.type === 'IntentRequest' && request.intent.name === 'AMAZON.YesIntent';
+    },
+    async handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        sessionAttributes.gameState = 'STARTED';
+        sessionAttributes.currentNode = 'story';
+
+        let data = await dbGet(params);
+        const converted = AWS.DynamoDB.Converter.unmarshall(data.Item);
+        const tree = new TreeModel();
+        let root = tree.parse(converted);
+
+        return responseBuilder
+            .speak(root.model.speak)
+            .reprompt(root.model.speak.repromptSpeak)
+            .getResponse();
+    },
+};
+
+const NoIntent = {
+    canHandle(handlerInput) {
+        // only treat no as an exit when outside a game
+        let isCurrentlyPlaying = false;
+        const request = handlerInput.requestEnvelope.request;
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        if (sessionAttributes.gameState &&
+            sessionAttributes.gameState === 'STARTED') {
+            isCurrentlyPlaying = true;
+        }
+
+        return !isCurrentlyPlaying && request.type === 'IntentRequest' && request.intent.name === 'AMAZON.NoIntent';
+    },
+    async handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        sessionAttributes.endedSessionCount += 1;
+        sessionAttributes.gameState = 'ENDED';
+        attributesManager.setPersistentAttributes(sessionAttributes);
+
+        await attributesManager.savePersistentAttributes();
+
+        let data = await dbGet(params);
+        const converted = AWS.DynamoDB.Converter.unmarshall(data.Item);
+        const tree = new TreeModel();
+        let root = tree.parse(converted);
+
+        return responseBuilder.speak(root.model.initialEndSpeak).getResponse();
+    },
+};
+
+const UnhandledIntent = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput) {
+        const outputSpeech = 'Say yes to continue, or no to end the game.';
+        return handlerInput.responseBuilder
+            .speak(outputSpeech)
+            .reprompt(outputSpeech)
+            .getResponse();
+    },
+};
+
+/**
+ *
+ * @type {{canHandle(*): *, handle(*): Promise<*>}}
+ */
+const ChoiceIntent = {
+    canHandle(handlerInput) {
+
+        // handle numbers only during a game
+        let isCurrentlyPlaying = false;
+        const request = handlerInput.requestEnvelope.request;
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        if (sessionAttributes.gameState &&
+            sessionAttributes.gameState === 'STARTED') {
+            isCurrentlyPlaying = true;
+        }
+
+        return isCurrentlyPlaying && request.type === 'IntentRequest' && request.intent.name === 'ChoiceIntent';
+    },
+    async handle(handlerInput) {
+
+        let data = await dbGet(params);
+
+        const {requestEnvelope, attributesManager, responseBuilder} = handlerInput;
+
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        const converted = AWS.DynamoDB.Converter.unmarshall(data.Item);
+
+        const tree = new TreeModel();
+        let root = tree.parse(converted);
+
+        const nextNodeId = requestEnvelope.request.intent.slots.number.value;
+
+        const nextNode = root.first(function (node) {
+            return node.model.id.toString() === nextNodeId.toString();
+        });
+
+        if(nextNode == null) {
+
+            // this option is completely invalid (doesn't exist anywhere)
+            return responseBuilder
+                .speak(root.model.invalidOptionSpeak)
+                .reprompt(root.model.repromptSpeak)
+                .getResponse();
+        } else {
+
+            let currentNodeId = sessionAttributes.currentNode;
+
+            const currentNode = root.first(function (node) {
+                return node.model.id.toString() === currentNodeId.toString();
+            });
+
+            if(currentNode.hasChildren()) {
+
+                // search from the current node
+                const options = currentNode.first(function(node) {
+
+                    if(node.isRoot()) {
+                        // this is the root node
+                        return false;
+                    } else {
+                        // check the node's parent is the current node
+                        return node.parent.model.id.toString() === currentNode.model.id.toString()
+                            && node.model.id.toString() ===  nextNodeId.toString();
+                    }
+                });
+
+                if(!options) { // this option doesn't exist as a child
+                    return responseBuilder
+                        .speak(root.model.invalidMoveSpeak)
+                        .reprompt(currentNode.model.speak)
+                        .getResponse();
+                } else {
+
+                    // player has moved
+                    sessionAttributes.currentNode = nextNodeId;
+
+                    let finishedSpeak = nextNode.hasChildren() ? "" : " " + root.model.finishedGameSpeak;
+
+                    return responseBuilder
+                        .speak(nextNode.model.speak + finishedSpeak)
+                        .reprompt("You have finished the adventure.")
+                        .getResponse();
+                }
+
+            } else {
+                return responseBuilder
+                    .speak(root.model.finishedGameSpeak)
+                    .reprompt(root.model.finishedGameSpeak)
+                    .getResponse();
+            }
+        }
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand the command. Please say again.')
+            .reprompt('Sorry, I can\'t understand the command. Please say again.')
+            .getResponse();
+    },
+};
+
+const FallbackHandler = {
+    // 2018-May-01: AMAZON.FallackIntent is only currently available in en-US locale.
+    //              This handler will not be triggered except in that locale, so it can be
+    //              safely deployed for any locale.
+    canHandle(handlerInput) {
+        // handle fallback intent, yes and no when playing a game
+        // for yes and no, will only get here if and not caught by the normal intent handler
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'AMAZON.FallbackIntent' ||
+                request.intent.name === 'AMAZON.YesIntent' ||
+                request.intent.name === 'AMAZON.NoIntent');
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+
+        if (sessionAttributes.gameState &&
+            sessionAttributes.gameState === 'STARTED') {
+            // currently playing
+
+            return handlerInput.responseBuilder
+                .speak(FALLBACK_MESSAGE_DURING_GAME)
+                .reprompt(FALLBACK_REPROMPT_DURING_GAME)
+                .getResponse();
+        }
+
+        // not playing
+        return handlerInput.responseBuilder
+            .speak(FALLBACK_MESSAGE_OUTSIDE_GAME)
+            .reprompt(FALLBACK_REPROMPT_OUTSIDE_GAME)
+            .getResponse();
+    },
+};
+
+const skillBuilder = Alexa.SkillBuilders.standard();
+
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchRequest,
+        ExitHandler,
+        SessionEndedRequest,
+        HelpIntent,
+        YesIntent,
+        NoIntent,
+        ChoiceIntent,
+        FallbackHandler,
+        UnhandledIntent,
+    )
+    .addErrorHandlers(ErrorHandler)
+    .withTableName('Pick-A-Path') // table is created in eu-west-1!
+    .withAutoCreateTable(true)
+    .lambda();
